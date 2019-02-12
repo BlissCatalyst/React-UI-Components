7c8c2da98950a55b7f0fc41e8e9a4af041dc1dd4
Logan Hufstetler <logan.hufstetler@gmail.com>
2019-02-12 15:04:23 -0700

    added all buttons to calculator.

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -2,12 +2,38 @@ import React from 'react';
 import './App.css';
 import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
 import ActionButton from './components/ButtonComponents/ActionButton';
+import NumberButton from './components/ButtonComponents/NumberButton';
 
 const App = () => {
   return (
     <div className="calculator">
-      <CalculatorDisplay />
-      <ActionButton />
+      <p><CalculatorDisplay /></p>
+      <p>
+        <ActionButton buttonStyle="clear" text="clear"/>
+        <NumberButton buttonStyle="operation" text="÷"/>
+      </p>
+      <p>
+        <NumberButton buttonStyle="number" text="7"/>
+        <NumberButton buttonStyle="number" text="8"/>
+        <NumberButton buttonStyle="number" text="9"/>
+        <NumberButton buttonStyle="operation" text="×"/>
+      </p>
+      <p>
+        <NumberButton buttonStyle="number" text="4"/>
+        <NumberButton buttonStyle="number" text="5"/>
+        <NumberButton buttonStyle="number" text="6"/>
+        <NumberButton buttonStyle="operation" text="-"/>
+      </p>
+      <p>
+        <NumberButton buttonStyle="number" text="1"/>
+        <NumberButton buttonStyle="number" text="2"/>
+        <NumberButton buttonStyle="number" text="3"/>
+        <NumberButton buttonStyle="operation" text="+"/>
+      </p>
+      <p>
+        <ActionButton buttonStyle="zero" text="0"/>
+        <NumberButton buttonStyle="operation" text="="/>
+      </p>
     </div>
   );
 };
--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -3,7 +3,7 @@ import './Button.css';
 
 const ActionButton = props => {
     return (
-        <button className="ActionButton">clear</button>
+        <button className={props.buttonStyle}>{props.text}</button>
     );
 };
 
